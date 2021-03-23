f752d311d38a945d5ebb0d50ccc947b9f5caa271
Nick <dohp.kim@coinvest.kr>
2021-03-23 18:10:46 +0900

    WIP: test-view

---
+++ src/main/java/my/demo/tradingview/controller/TestController.java
@@ -0,0 +1,20 @@
+package my.demo.tradingview.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@RequiredArgsConstructor
+@Controller
+public class TestController {
+
+  private final SocketBinaryHandler socketBinaryHandler;
+
+  @GetMapping("/test")
+  public String test() {
+    return "protocolbuf";
+  }
+
+}
